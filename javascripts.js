67a165fedffef9b65673bb37f686da1fe9bf749b
SHIVAM YADAV <demo@gmail.com>
2023-08-05 23:38:00 +0530

    new update

--- javascripts.js
+++ javascripts.js
@@ -1,25 +1,10 @@
 const url = "https://api.openweathermap.org/data/2.5/weather?q=agra&APPID=5877846f0524ff38ab7a9e4bdc96a0fb";
 
-const butt = document.getElementById("mybtn");
+const btnn = document.getElementsByTagName("button")[0];
+console.log(btnn);
 
-const htag = document.getElementsByTagName("h1")[0];
-console.log(htag);
-
-
-function fetching() {
-    const response = fetch(url);
-    const data = response.json();
+btnn.addEventListener("click",async()=>{
+    const response=await fetch(url);
+    const data=await response.json();
     console.log(data);
-    htag.innerHTML = data;
-    htag.innerText = data;
-}
-
-butt.addEventListener("click", ()=>{
-    fetching;
 });
-
-
-
-
-// const input=document.getElementById("form-datas");
-// console.log(input);
